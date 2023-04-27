34835aa98d3efce03e88b9aff0bf063cc3161c66
max <93793772+maxhithere@users.noreply.github.com>
2023-04-26 22:07:08 -0400

    new methods

--- Array.lua
+++ Array.lua
@@ -155,3 +155,120 @@ function Array:shuffle()
     self.values[i], self.values[j] = self.values[j], self.values[i]
   end
 end
+
+function Array:every(fn)
+  for i, value in ipairs(self.values) do
+    if not fn(value) then
+      return false
+    end
+  end
+  return true
+end
+
+function Array:flat(depth)
+  depth = depth or 1
+  local flattened = {}
+  local function flattenHelper(arr, currentDepth)
+    if currentDepth == 0 then
+      for _, value in ipairs(arr) do
+        table.insert(flattened, value)
+      end
+    else
+      for _, value in ipairs(arr) do
+        if type(value) == "table" then
+          flattenHelper(value, currentDepth - 1)
+        else
+          table.insert(flattened, value)
+        end
+      end
+    end
+  end
+  flattenHelper(self.values, depth)
+  return Array.new(flattened)
+end
+
+function Array:flatMap(fn)
+  local mapped = {}
+  for i, value in ipairs(self.values) do
+    local result = fn(value, i, self.values)
+    if type(result) == "table" then
+      for _, mappedValue in ipairs(result) do
+        table.insert(mapped, mappedValue)
+      end
+    else
+      table.insert(mapped, result)
+    end
+  end
+  return Array.new(mapped)
+end
+
+function Array:last()
+  return self.values[#self.values]
+end
+
+function Array:first()
+  return self.values[1]
+end
+
+function Array:toString()
+  local str = "["
+  for i, value in ipairs(self.values) do
+    if i > 1 then
+      str = str .. ", "
+    end
+    if type(value) == "string" then
+      str = str .. "\"" .. value .. "\""
+    else
+      str = str .. tostring(value)
+    end
+  end
+  str = str .. "]"
+  return str
+end
+
+function Array:unique()
+  local unique = {}
+  for i, value in ipairs(self.values) do
+    if not unique[value] then
+      unique[value] = true
+    end
+  end
+  local result = {}
+  for value, _ in pairs(unique) do
+    table.insert(result, value)
+  end
+  return Array.new(result)
+end
+
+function Array:splice(start, deleteCount, ...)
+  local deleted = {}
+  local valuesToAdd = {...}
+  local numValuesToAdd = #valuesToAdd
+  local numValuesToDelete = deleteCount or 0
+  local numValues = #self.values
+  local endOfDeleted = start + numValuesToDelete - 1
+  for i = start, endOfDeleted do
+    table.insert(deleted, self.values[i])
+  end
+  if numValuesToAdd > numValuesToDelete then
+    local diff = numValuesToAdd - numValuesToDelete
+    for i = numValues, start, -1 do
+      local newIdx = i + diff
+      self.values[newIdx] = self.values[i]
+    end
+  elseif numValuesToAdd < numValuesToDelete then
+    local diff = numValuesToDelete - numValuesToAdd
+    for i = start, numValues - diff do
+      local oldIdx = i + diff
+      self.values[i] = self.values[oldIdx]
+    end
+    for i = numValues, numValues - diff + 1, -1 do
+      self.values[i] = nil
+    end
+  end
+  for i = 1, numValuesToAdd do
+    local newIdx = start + i - 1
+    self.values[newIdx] = valuesToAdd[i]
+  end
+  return Array.new(deleted)
+end
